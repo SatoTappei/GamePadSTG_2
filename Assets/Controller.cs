55278b22e0ef0470bb8909b1508c5c9b406213ff
SatouTappei <stutpi.signup@gmail.com>
2022-09-19 15:15:11 +0900

    初コミット

---
+++ Assets/Controller.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// プレイヤーのコントローラー
+/// </summary>
+public class Controller : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
