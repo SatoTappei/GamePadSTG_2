659ccb4ca97e9116af940c060e12d01a60aa5121
SatouTappei <stutpi.signup@gmail.com>
2022-10-22 23:54:46 +0900

    区画と区域のクラスにメソッドを追加

--- Assets/MapGenerateTest/MapGenerateUtility.cs
+++ Assets/MapGenerateTest/MapGenerateUtility.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 /// <summary>マップ上の1マス</summary>
 public class Mass
 {
-    int _x;
-    int _z;
-    public char _char;
+    /// <summary>マップ上での座標、区画内での番号とは違うので注意</summary>
+    (int z, int x) _pos;
+    char _char;
+
+    public (int z, int x) Pos { get => _pos; }
+    public char Char { get => _char; set => _char = value; }
 }
 
 /// <summary>区域を構成する区画</summary>
 public class Section
 {
-    int _height;
-    int _widht;
+    readonly int _height;
+    readonly int _widht;
     Mass[,] _masses;
 
     public Section(int height, int width)
@@ -22,23 +25,50 @@ public class Section
         _masses = new Mass[height, width];
     }
 
-    /// <summary>この区画の文字を二次元配列にして返す</summary>
+    public Mass[,] Masses { get => _masses; }
+    public int Height { get => _height; }
+    public int Widht { get => _widht; }
+
+    /// <summary>区画内の番号を渡すと対応した対応したマスを返す</summary>
+    public Mass GetMass(int z, int x) => _masses[z, x];
+
+    /// <summary>この区画を文字列二次元配列にして返す。</summary>
     public char[,] GetStringArray()
     {
         char[,] array = new char[_height, _widht];
         for (int z = 0; z < _height; z++)
             for (int x = 0; x < _widht; x++)
             {
-                array[z, x] = _masses[z, x]._char;
+                array[z, x] = _masses[z, x].Char;
             }
 
         return array;
+    }
+
+    /// <summary>文字列二次元配列を区画に反映させる</summary>
+    public void SetStringArray(char[,] array)
+    {
+        if (array.GetLength(0) != _height || 
+            array.GetLength(1) != _widht)
+        {
+            Debug.LogWarning("渡された配列が区画の大きさと違います。");
+            return;
+        }
+
+        for (int z = 0; z < _height; z++)
+            for (int x = 0; x < _widht; x++)
+            {
+                _masses[z, x].Char = array[z, x];
+            }
     }
 }
 
 /// <summary>マップを構成する区域</summary>
 public class Area
 {
+    readonly int _height = 7;
+    readonly int _widht = 7;
+
     /// <summary>各区画はテンキーの番号に対応している</summary>
     Section[] _sections = new Section[]
     {
@@ -56,19 +86,26 @@ public class Area
     // 各区画を合体させて1つの文字列型の二次元配列にして返す
     public char[,] GetStringArray()
     {
-        // このメソッドを呼び出して返した二次元配列をもとに
-        // マップをオブジェクトとして生成することを留意する
-        // TODO:ここから
-        char[,] mapArray = new char[7, 7];
-        for (int i = 0; i < 7; i++)
-            for (int j = 0; j < 7; j++)
-                mapArray[i, j] = 'r';
-        // ここまでを書き直す
+        char[,] mapArray = new char[_height, _widht];
+        foreach (Section sct in _sections)
+        {
+            for (int z = 0; z < sct.Height; z++)
+                for (int x = 0; x < sct.Widht; x++)
+                {
+                    // 区画内の各マスをマップの文字列二次元配列に反映する
+                    Mass mass = sct.GetMass(z, x);
+                    mapArray[mass.Pos.z, mass.Pos.x] = mass.Char;
+                }
+        }
+
         return mapArray;
     }
 }
 
-/// <summary>マップ</summary>
+/// <summary>
+/// 多数の区域からなるマップ
+/// 生成は区域ごとに行うのでマップクラスには文字列二次元配列は持たない
+/// </summary>
 public class Map
 {
     public Area[,] Areas { get; set; }
